faa7f7c7333aed26a51e7fa022f09bebce3a506d
EamonBachari <eamonbachari@gmail.com>
2017-07-20 12:22:46 -0500

    Deck class fully functional.

--- lib/card.rb
+++ lib/card.rb
@@ -1,36 +1,32 @@
 class Card
-  attr_accessor :rank, :suit, :value, :suit_value
+  attr_reader :rank, :suit, :value, :suit_value
 
   def initialize(rank, suit)
     case rank
     when :A
-      value = 1
+      @value = 1
     when :K
-      value = 13
+      @value = 13
     when :Q
-      value = 12
+      @value = 12
     when :J
-      value = 11
+      @value = 11
     else
-      value = rank.to_i
+      @value = rank.to_i
     end
 
     case suit
     when :hearts
-      suit_value = 4
+      @suit_value = 4
     when :diamonds
-      suit_value = 3
+      @suit_value = 3
     when :clubs
-      suit_value = 2
+      @suit_value = 2
     when :spades
-      suit_value = 1
+      @suit_value = 1
     end
-
-
     @rank = rank
     @suit = suit
-    @value = value
-    @suit_value = suit_value
   end
 
   def greater_than?(card)
@@ -45,4 +41,7 @@ class Card
     greater_than
   end
 
+  def ==(other)
+    @rank == other.rank && @suit == other.suit
+  end
 end
--- lib/deck.rb
+++ lib/deck.rb
@@ -1,4 +1,24 @@
 require_relative "card"
 
 class Deck
+  def initialize
+    @cards = []
+    [:clubs, :diamonds, :hearts, :spades].each do |suit|
+      [:A, 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K].each do |rank|
+        @cards << Card.new(rank, suit)
+      end
+    end
+  end
+
+  def cards_left
+    @cards.length
+  end
+
+  def shuffle
+    @cards.shuffle
+  end
+
+  def draw
+    @cards.shift
+  end
 end
