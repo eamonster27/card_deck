cd81b1d68ab7829740cd05345847300ddb58a503
EamonBachari <eamonbachari@gmail.com>
2017-07-20 11:13:51 -0500

    Card class functional.

---
+++ lib/card.rb
@@ -0,0 +1,48 @@
+class Card
+  attr_accessor :rank, :suit, :value, :suit_value
+
+  def initialize(rank, suit)
+    case rank
+    when :A
+      value = 1
+    when :K
+      value = 13
+    when :Q
+      value = 12
+    when :J
+      value = 11
+    else
+      value = rank.to_i
+    end
+
+    case suit
+    when :hearts
+      suit_value = 4
+    when :diamonds
+      suit_value = 3
+    when :clubs
+      suit_value = 2
+    when :spades
+      suit_value = 1
+    end
+
+
+    @rank = rank
+    @suit = suit
+    @value = value
+    @suit_value = suit_value
+  end
+
+  def greater_than?(card)
+    greater_than = false
+    if(@value > card.value)
+      greater_than = true
+    elsif(@value == card.value)
+      if(@suit_value > card.suit_value)
+        greater_than = true
+      end
+    end
+    greater_than
+  end
+
+end
